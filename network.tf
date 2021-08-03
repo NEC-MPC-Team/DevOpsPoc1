3f6a170aad0299a48ea9e0ebc98242b76b445e37
ayildirimnec <87165930+ayildirimnec@users.noreply.github.com>
2021-08-04 09:55:41 +1000

    removed the extra curly brace
    removed the extra curly brace

--- network.tf
+++ network.tf
@@ -19,7 +19,7 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-  }
+  
 
   subnet {
     name           = "subnet1"
