a478c3ee4a86115bc3086a4af023face7a9355aa
ayildirimnec <87165930+ayildirimnec@users.noreply.github.com>
2021-08-04 09:53:17 +1000

    fix on the missing ddos reference

--- network.tf
+++ network.tf
@@ -19,10 +19,6 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  ddos_protection_plan {
-    id     = azurerm_network_ddos_protection_plan.example.id
-    enable = true
   }
 
   subnet {
