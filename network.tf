c52f161b9044d702e12fc0de8c95af76203c6e1d
ayildirimnec <87165930+ayildirimnec@users.noreply.github.com>
2021-08-27 09:32:42 +1000

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,6 +2,10 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {}
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "DevOpsPOC"
   location = "Australia East"
@@ -13,23 +17,13 @@ resource "azurerm_network_security_group" "example" {
   resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_network_ddos_protection_plan" "example" {
-  name                = "ddospplan1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
 resource "azurerm_virtual_network" "example" {
   name                = "virtualNetwork1"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  ddos_protection_plan {
-    id     = azurerm_network_ddos_protection_plan.example.id
-    enable = true
-  }
+  
 
   subnet {
     name           = "subnet1"
@@ -45,11 +39,6 @@ resource "azurerm_virtual_network" "example" {
     name           = "subnet3"
     address_prefix = "10.0.3.0/24"
     security_group = azurerm_network_security_group.example.id
-  }
-  
-    subnet {
-    name           = "subnet4"
-    address_prefix = "10.0.4.0/24"
   }
 
   tags = {
